c4ae437d10df6ccbb63c49297384bd4a491f2735
lzanchin <leandro.zanchin@gmail.com>
2022-08-04 16:09:36 +1000

    Add game function

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ function getUserChoice() {
         option = prompt("Type your option: ROCK, PAPER or SCISSORS");
         option = option.toUpperCase();        
     } while (option != "ROCK" && option != "PAPER" && option != "SCISSORS");
+    console.log("Player chooses: " + option);
     return option;
 }
 
@@ -14,14 +15,18 @@ function getComputerChoice() {
     // get a random number between 1 and 3    
     let number = Math.floor(Math.random() * (3-1 + 1) + 1);
     
-    // depending on the number, get a different choice    
+    // depending on the number, get a different choice
+    let choice;    
     if (number == 1) {
-        return "ROCK";        
+        choice = "ROCK";        
     } else if (number == 2) {
-        return "PAPER";
+        choice = "PAPER";
     } else {
-        return "SCISSORS";
+        choice = "SCISSORS";
     }
+    choice = "ROCK";
+    console.log("Computer chooses: " + choice);    
+    return choice;
 
     // test return values
     // return "ROCK"
@@ -47,4 +52,45 @@ function playRound(playerSelection, computerSelection){
     return result;    
 }
 
-console.log(playRound(getUserChoice(), getComputerChoice()));
+function displayResults(playerScore, computerScore){
+    console.log("........Checking results........")
+    console.log("Player Score: " + playerScore)
+    console.log("Computer Score: " + computerScore);        
+    if (playerScore > computerScore) {
+        console.log("YOU WON!!");        
+    } else if (playerScore < computerScore) {
+        console.log("YOU LOST!! Best luck next time.");
+    } else {
+        console.log("THE GAME FINISHED WITH A DRAW.")
+    }
+}
+
+function game(){
+    let playerScore = 0;
+    let computerScore = 0;    
+    let roundsLeft = 5;
+    let roundResult;
+    console.log("........Starting a new game........")
+    for (let i = 1; i < 6; i++) {                
+        console.log("========== Round " + i + " ==========");
+        // if the game continues, then play another round
+        roundResult =playRound(getUserChoice(), getComputerChoice());
+        if (roundResult.includes("You win")) {
+            playerScore++;            
+        } else if (roundResult.includes("You lost")) {
+            computerScore++;            
+        } else {
+            console.log("Draw");
+        }
+        console.log("Player Score: " + playerScore)
+        console.log("Computer Score: " + computerScore);
+
+        // check if the game is finished        
+        if (roundsLeft <= (playerScore - computerScore) || roundsLeft <= (computerScore - playerScore)) {
+            break;
+        }
+        roundsLeft--;        
+    }
+    displayResults(playerScore, computerScore);    
+}
+game();
