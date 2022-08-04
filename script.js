3891533b2d7910a230338b94b25ec9d7cb57b150
lzanchin <leandro.zanchin@gmail.com>
2022-08-04 11:46:35 +1000

    Add PlayRound

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-let choice;
+let playerSelection;
+let computerSelection;
 
 function getUserChoice() {
     let option;
@@ -9,5 +10,41 @@ function getUserChoice() {
     return option;
 }
 
-choice = getUserChoice();
-console.log(choice);
+function getComputerChoice() {
+    // get a random number between 1 and 3    
+    let number = Math.floor(Math.random() * (3-1 + 1) + 1);
+    
+    // depending on the number, get a different choice    
+    if (number == 1) {
+        return "ROCK";        
+    } else if (number == 2) {
+        return "PAPER";
+    } else {
+        return "SCISSORS";
+    }
+
+    // test return values
+    // return "ROCK"
+}
+
+function playRound(playerSelection, computerSelection){
+    let result;
+    if (playerSelection == computerSelection) {
+        result = "Draw";
+    } else if (playerSelection == "ROCK" && computerSelection == "SCISSORS") {
+        result = "You win, " + playerSelection + " beats " + computerSelection;
+    } else if (playerSelection == "ROCK" && computerSelection == "PAPER") {
+        result = "You lost, " + computerSelection + " beats " + playerSelection;
+    } else if (playerSelection == "PAPER" && computerSelection == "ROCK"){
+        result = "You win, " + playerSelection + " beats " + computerSelection;
+    } else if (playerSelection == "PAPER" && computerSelection == "SCISSORS"){
+        result = "You lost, " + computerSelection + " beats " + playerSelection;
+    } else if (playerSelection == "SCISSORS" && computerSelection == "PAPER"){
+        result = "You win, " + playerSelection + " beats " + computerSelection;
+    } else if (playerSelection == "SCISSORS" && computerSelection == "ROCK"){
+        result = "You lost, " + computerSelection + " beats " + playerSelection;
+    }
+    return result;    
+}
+
+console.log(playRound(getUserChoice(), getComputerChoice()));
